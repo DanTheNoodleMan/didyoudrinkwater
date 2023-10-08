191dd4c3f5bec154a0a5e76d525aef1645674efe
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-10-08 15:01:33 -0400

    Added input to write age and save to global state

--- app/AgeScreen.tsx
+++ app/AgeScreen.tsx
@@ -1,14 +1,50 @@
 import React from "react";
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {
+    View,
+    Text,
+    Button,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+} from "react-native";
 
-function AgeScreen({navigation}) {
-    const [age, setAge] = React.useState(0 as number);
+import { FormContext } from "./FormContext";
+
+function AgeScreen({ navigation }) {
+    const { age, setAge } = React.useContext(FormContext);
+
+    const handleAgeChange = (text: string) => {
+      const parsedAge = parseInt(text, 10);
+      if (!isNaN(parsedAge)) {
+        setAge(parsedAge);
+      }
+    };
+
     return (
-        <View>
-          {/* Render your UI for SexScreen */}
-          <Button title="Next" onPress={() => navigation.navigate('WeightScreen')} />
-        </View>
+        <SafeAreaView>
+            <TextInput
+                style={styles.input}
+                onChangeText={handleAgeChange}
+                value={age ? age.toString() : ""}
+                placeholder="Enter your age"
+                keyboardType="numeric"
+            />
+            <Button
+                title="Next"
+                onPress={() => navigation.navigate("WeightScreen")}
+                disabled={!age}
+            />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
 
 export default AgeScreen;
